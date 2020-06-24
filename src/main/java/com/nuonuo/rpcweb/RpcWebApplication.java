bcdf5e2111733738d6e4cea857c4e7a6538d2d30
lindong <lindong>
2020-06-24 14:02:36 +0800

    commit

---
+++ src/main/java/com/nuonuo/rpcweb/RpcWebApplication.java
@@ -0,0 +1,13 @@
+package com.nuonuo.rpcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuonuo/rpcweb/RpcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuonuo.rpcweb;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
